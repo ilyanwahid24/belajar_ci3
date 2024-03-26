3bf59bfddbe999403470c8cd019930fbacc4c3be
ilyanwahid24 <illyyanwahid24@gmail.com>
2024-03-26 07:48:14 +0700

    first commit

---
+++ application/controllers/NewController.php
@@ -0,0 +1,6 @@
+<?php
+class NewController extends CI_Controller {
+    public function index(){
+    $this->load->view('perkenalkan');
+    }
+}
---
+++ application/views/perkenalkan.php
@@ -0,0 +1,5 @@
+<?php
+        echo "<h1>Perkenalkan</h1>";
+        echo "Nama saya Ilyan Wahid
+        Saya tinggal di daerah PIK. olaharga yang saya sukai adalah Golf.";
+    
